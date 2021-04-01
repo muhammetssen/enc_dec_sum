9ef942d0aacade4e58c582fd0fa20cee15a8e47b
Batuhan Baykara <bbaykara@levrek1.yonetim>
2021-04-01 16:08:46 +0300

    aded base

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,6 @@
 from transformers import PyTorchBenchmark, PyTorchBenchmarkArguments
 
-args = PyTorchBenchmarkArguments(models=["google/mt5-small","facebook/mbart-large-cc25"], batch_sizes=[4, 8, 16, 32],
+args = PyTorchBenchmarkArguments(models=["google/mt5-small","google/mt5-base","facebook/mbart-large-cc25"], batch_sizes=[2, 4, 8],
                                  sequence_lengths=[128, 256, 512, 1024], speed=False)
 benchmark = PyTorchBenchmark(args)
 results = benchmark.run()
