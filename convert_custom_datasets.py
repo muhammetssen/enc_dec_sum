fe0e1ea729be66d5663457dabb23613df384fde6
Batuhan Baykara <batuhanbaykara@yahoo.com>
2021-04-03 23:18:14 +0300

    disabled column dropping

--- convert_custom_datasets.py
+++ convert_custom_datasets.py
@@ -4,9 +4,11 @@ train = pd.read_csv("data/tr_news_raw/train.tsv", sep='\t')
 validation = pd.read_csv("data/tr_news_raw/validation.tsv", sep='\t')
 test = pd.read_csv("data/tr_news_raw/test.tsv", sep='\t')
 
+'''
 train.drop(train.columns.difference(['content', 'abstract']), 1, inplace=True)
 validation.drop(validation.columns.difference(['content', 'abstract']), 1, inplace=True)
 test.drop(test.columns.difference(['content', 'abstract']), 1, inplace=True)
+'''
 
 train.to_csv("data/tr_news_raw/train.csv", index=False)
 validation.to_csv("data/tr_news_raw/validation.csv", index=False)
