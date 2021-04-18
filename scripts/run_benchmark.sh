ad475b1cd926c0e93580db587ab1956ae91b53d6
Batuhan Baykara <batuhanbaykara@yahoo.com>
2021-04-18 08:26:34 +0000

    title scripts added

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#SBATCH -p akya-cuda
-#SBATCH -A bbaykara
-#SBATCH -J mt5
+#SBATCH -p long
+#SBATCH -J gpu_benchmark
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -c 40
-#SBATCH --gres=gpu:4
-#SBATCH --time=7-00:00:00
-#SBATCH --mail-type=ALL
+#SBATCH -c 10
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=v100
+#SBATCH --time=1-00:00:00
 
-module load centos7.3/lib/cuda/10.1
+module load cuda-10.2.89-gcc-10.2.0-dgnsc3t
 
 echo "SLURM_NODELIST $SLURM_NODELIST"
 echo "NUMBER OF CORES $SLURM_NTASKS"
 echo "CUDA DEVICES $CUDA_VISIBLE_DEVICES"
-/truba/home/bbaykara/code/enc_dec_sum/venv/bin/python /truba/home/bbaykara/code/enc_dec_sum/benchmark.py
+/cta/users/bbaykara/code/enc_dec_sum/venv/bin/python /cta/users/bbaykara/code/enc_dec_sum/benchmark.py
