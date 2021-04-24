1378dc911deadaf89c234b6c3911c69223f20c46
Batuhan Baykara <batuhanbaykara@yahoo.com>
2021-04-24 12:20:29 +0000

    benchmark config change and removed outputs from git

--- benchmark.py
+++ benchmark.py
@@ -1,7 +1,7 @@
 from transformers import PyTorchBenchmark, PyTorchBenchmarkArguments
 
-args = PyTorchBenchmarkArguments(models=["google/mt5-base","facebook/mbart-large-cc25","dbmdz/bert-base-turkish-uncased","bert-base-multilingual-uncased"], batch_sizes=[8, 16, 32,64],
-                                 sequence_lengths=[32,64,256,512], speed=False)
+args = PyTorchBenchmarkArguments(models=["google/mt5-base","facebook/mbart-large-cc25","dbmdz/bert-base-turkish-uncased","bert-base-multilingual-uncased"], batch_sizes=[2,4,8, 16],
+                                 sequence_lengths=[32,64,256,512], speed=False, training=True)
 benchmark = PyTorchBenchmark(args)
 results = benchmark.run()
 print(results)
