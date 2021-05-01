91d92128a564ad680285b51eb016f099177bc877
Batuhan Baykara <batuhanbaykara@yahoo.com>
2021-05-01 22:14:31 +0300

    uncomment cuda module load

--- scripts/tr_news/summary/calculate_rouge_tr_news_mt5_summary.sh
+++ scripts/tr_news/summary/calculate_rouge_tr_news_mt5_summary.sh
@@ -9,7 +9,7 @@
 #SBATCH --time=1-00:00:00
 #SBATCH --mail-type=ALL
 
-#module load centos7.3/lib/cuda/10.1
+module load centos7.3/lib/cuda/10.1
 
 echo "SLURM_NODELIST $SLURM_NODELIST"
 echo "NUMBER OF CORES $SLURM_NTASKS"
