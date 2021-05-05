f5d8efe5a6b3b88f3de36339783b08490c4ff292
Batuhan Baykara <batuhanbaykara@yahoo.com>
2021-05-06 00:01:13 +0300

    checkpoint eval

--- scripts/ml_sum/title/calculate_rouges_ml_sum_title.sh
+++ scripts/ml_sum/title/calculate_rouges_ml_sum_title.sh
@@ -23,75 +23,79 @@ TASK_NAME=title
 #for MODEL_NAME in ml_sum_mt5_title ml_sum_mbart_title ml_sum_berturk32k_title ml_sum_berturk32k_cased_title ml_sum_mbert_uncased_title ml_sum_mbert_cased_title
 for MODEL_NAME in ml_sum_mt5_title ml_sum_berturk32k_title ml_sum_berturk32k_cased_title ml_sum_mbert_uncased_title ml_sum_mbert_cased_title
 do
-  mkdir -p $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME
-  echo $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME
+  for MODEL_NAME in $(find $HOME_DIR/$MODELS_FOLDER/$MODEL_NAME -maxdepth 1 -type d)
+  do
+    MODEL_NAME=${MODEL_NAME#$HOME_DIR/$MODELS_FOLDER/}
+    mkdir -p $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME
+    echo $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME
 
-  if [[ "$MODEL_NAME" == *"mt5"* ]]; then
-    echo "mt5"
-    $HOME_DIR/venv/bin/python $HOME_DIR/post_metrics.py \
-    --model_name_or_path $HOME_DIR/$MODELS_FOLDER/$MODEL_NAME \
-    --dataset_name mlsum \
-    --dataset_version tu \
-    --text_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/text_outputs.csv \
-    --rouge_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/rouge_outputs.json \
-    --novelty_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/novely_outputs.json \
-    --do_tr_lowercase True \
-    --source_column_name summary \
-    --target_column_name title \
-    --source_prefix "summarize: " \
-    --num_beams 4 \
-    --ngram_blocking_size 3 \
-    --early_stopping True \
-    --use_cuda False \
-    --max_source_length 256 \
-    --max_target_length 64 \
-    --batch_size 16 \
-    --use_stemmer_in_rouge False
-  fi
-  if [[ "$MODEL_NAME" == *"mbart"* ]]; then
-    echo "mbart"
-    $HOME_DIR/venv/bin/python $HOME_DIR/post_metrics.py \
-    --model_name_or_path $HOME_DIR/$MODELS_FOLDER/$MODEL_NAME \
-    --dataset_name mlsum \
-    --dataset_version tu \
-    --text_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/text_outputs.csv \
-    --rouge_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/rouge_outputs.json \
-    --novelty_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/novely_outputs.json \
-    --do_tr_lowercase True \
-    --source_column_name summary \
-    --target_column_name title \
-    --source_prefix "" \
-    --num_beams 4 \
-    --ngram_blocking_size 3 \
-    --early_stopping True \
-    --use_cuda False \
-    --max_source_length 256 \
-    --max_target_length 64 \
-    --batch_size 16 \
-    --use_stemmer_in_rouge False
-  fi
-  if [[ "$MODEL_NAME" == *"bert"* ]]; then
-    echo "bert"
-    $HOME_DIR/venv/bin/python $HOME_DIR/post_metrics.py \
-    --model_name_or_path $HOME_DIR/$MODELS_FOLDER/$MODEL_NAME \
-    --dataset_name mlsum \
-    --dataset_version tu \
-    --text_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/text_outputs.csv \
-    --rouge_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/rouge_outputs.json \
-    --novelty_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/novely_outputs.json \
-    --do_tr_lowercase True \
-    --source_column_name summary \
-    --target_column_name title \
-    --source_prefix "" \
-    --num_beams 4 \
-    --ngram_blocking_size 3 \
-    --early_stopping True \
-    --use_cuda False \
-    --max_source_length 256 \
-    --max_target_length 64 \
-    --batch_size 16 \
-    --use_stemmer_in_rouge False
-  fi
+    if [[ "$MODEL_NAME" == *"mt5"* ]]; then
+      echo "mt5"
+      $HOME_DIR/venv/bin/python $HOME_DIR/post_metrics.py \
+      --model_name_or_path $HOME_DIR/$MODELS_FOLDER/$MODEL_NAME \
+      --dataset_name mlsum \
+      --dataset_version tu \
+      --text_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/text_outputs.csv \
+      --rouge_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/rouge_outputs.json \
+      --novelty_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/novely_outputs.json \
+      --do_tr_lowercase True \
+      --source_column_name summary \
+      --target_column_name title \
+      --source_prefix "summarize: " \
+      --num_beams 4 \
+      --ngram_blocking_size 3 \
+      --early_stopping True \
+      --use_cuda False \
+      --max_source_length 256 \
+      --max_target_length 64 \
+      --batch_size 16 \
+      --use_stemmer_in_rouge False
+    fi
+    if [[ "$MODEL_NAME" == *"mbart"* ]]; then
+      echo "mbart"
+      $HOME_DIR/venv/bin/python $HOME_DIR/post_metrics.py \
+      --model_name_or_path $HOME_DIR/$MODELS_FOLDER/$MODEL_NAME \
+      --dataset_name mlsum \
+      --dataset_version tu \
+      --text_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/text_outputs.csv \
+      --rouge_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/rouge_outputs.json \
+      --novelty_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/novely_outputs.json \
+      --do_tr_lowercase True \
+      --source_column_name summary \
+      --target_column_name title \
+      --source_prefix "" \
+      --num_beams 4 \
+      --ngram_blocking_size 3 \
+      --early_stopping True \
+      --use_cuda False \
+      --max_source_length 256 \
+      --max_target_length 64 \
+      --batch_size 16 \
+      --use_stemmer_in_rouge False
+    fi
+    if [[ "$MODEL_NAME" == *"bert"* ]]; then
+      echo "bert"
+      $HOME_DIR/venv/bin/python $HOME_DIR/post_metrics.py \
+      --model_name_or_path $HOME_DIR/$MODELS_FOLDER/$MODEL_NAME \
+      --dataset_name mlsum \
+      --dataset_version tu \
+      --text_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/text_outputs.csv \
+      --rouge_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/rouge_outputs.json \
+      --novelty_outputs_file_path $HOME_DIR/$RESULTS_FOLDER/$DATASET_NAME/$TASK_NAME/$MODEL_NAME/novely_outputs.json \
+      --do_tr_lowercase True \
+      --source_column_name summary \
+      --target_column_name title \
+      --source_prefix "" \
+      --num_beams 4 \
+      --ngram_blocking_size 3 \
+      --early_stopping True \
+      --use_cuda False \
+      --max_source_length 256 \
+      --max_target_length 64 \
+      --batch_size 16 \
+      --use_stemmer_in_rouge False
+    fi
+  done
 done
 
 
