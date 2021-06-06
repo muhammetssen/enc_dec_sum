5b110016dea361f7d6698885e0821d0559ea288e
Batuhan Baykara <batuhanbaykara@yahoo.com>
2021-06-06 12:37:39 +0300

    correct the input of first 3

--- scripts/cross_dataset/calculate_cross_dataset_rouges_mt5_title.sh
+++ scripts/cross_dataset/calculate_cross_dataset_rouges_mt5_title.sh
@@ -79,44 +79,23 @@ do
     mkdir -p $OUTPUTS_PATH
     echo $OUTPUTS_PATH
 
-    if [[ "$DATASET_NAME" == *"tr_news"* ]]; then
-      $HOME_DIR/venv/bin/python $HOME_DIR/post_metrics.py \
-      --model_name_or_path $HOME_DIR/$MODELS_FOLDER/$MODEL_NAME \
-      --dataset_test_csv_file_path $HOME_DIR/data/$DATASET_NAME/test.csv \
-      --text_outputs_file_path $OUTPUTS_PATH/text_outputs.csv \
-      --rouge_outputs_file_path $OUTPUTS_PATH/rouge_outputs.json \
-      --novelty_outputs_file_path $OUTPUTS_PATH/novely_outputs.json \
-      --do_tr_lowercase True \
-      --source_column_name abstract \
-      --target_column_name title \
-      --source_prefix "summarize: " \
-      --num_beams 4 \
-      --ngram_blocking_size 3 \
-      --early_stopping False \
-      --use_cuda True \
-      --max_source_length 256 \
-      --max_target_length 64 \
-      --batch_size 16 \
-      --use_stemmer_in_rouge False
-    else
-      $HOME_DIR/venv/bin/python $HOME_DIR/post_metrics.py \
-      --model_name_or_path $HOME_DIR/$MODELS_FOLDER/$MODEL_NAME \
-      --dataset_test_csv_file_path $HOME_DIR/data/$DATASET_NAME/test.csv \
-      --text_outputs_file_path $OUTPUTS_PATH/text_outputs.csv \
-      --rouge_outputs_file_path $OUTPUTS_PATH/rouge_outputs.json \
-      --novelty_outputs_file_path $OUTPUTS_PATH/novely_outputs.json \
-      --do_tr_lowercase True \
-      --source_column_name summary \
-      --target_column_name title \
-      --source_prefix "summarize: " \
-      --num_beams 4 \
-      --ngram_blocking_size 3 \
-      --early_stopping False \
-      --use_cuda True \
-      --max_source_length 256 \
-      --max_target_length 64 \
-      --batch_size 16 \
-      --use_stemmer_in_rouge False
-    fi
+    $HOME_DIR/venv/bin/python $HOME_DIR/post_metrics.py \
+    --model_name_or_path $HOME_DIR/$MODELS_FOLDER/$MODEL_NAME \
+    --dataset_test_csv_file_path $HOME_DIR/data/$DATASET_NAME/test.csv \
+    --text_outputs_file_path $OUTPUTS_PATH/text_outputs.csv \
+    --rouge_outputs_file_path $OUTPUTS_PATH/rouge_outputs.json \
+    --novelty_outputs_file_path $OUTPUTS_PATH/novely_outputs.json \
+    --do_tr_lowercase True \
+    --source_column_name first3 \
+    --target_column_name title \
+    --source_prefix "summarize: " \
+    --num_beams 4 \
+    --ngram_blocking_size 3 \
+    --early_stopping False \
+    --use_cuda True \
+    --max_source_length 256 \
+    --max_target_length 64 \
+    --batch_size 16 \
+    --use_stemmer_in_rouge False
   done
 done
